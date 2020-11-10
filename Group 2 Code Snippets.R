477f4abd26eb19cea7f2d24443ddb9fe5b302ae7
bakerb545 <34195223+bakerb545@users.noreply.github.com>
2020-11-10 11:09:26 -0600

    In-Class Work for 11/10
    In-class work for 11/10, adjustments to group 2 code snippets, and lab 7

--- Group 2 Code Snippets.R
+++ Group 2 Code Snippets.R
@@ -7,33 +7,42 @@ library(readxl)
 bronx1<-read_xls("C:/Users/phoen/Downloads/rollingsales_bronx.xls", sheet=1,skip=4) 
 
 View(bronx1)
-attach(bronx1)
 `SALE\nPRICE`<- sub("\\$", "", `SALE\nPRICE`)
 `SALE\nPRICE`<- as.numeric(gsub(",", "", `SALE\nPRICE`))
 `GROSS SQUARE FEET`<-as.numeric(gsub(",","", `GROSS SQUARE FEET`)) 
 `LAND SQUARE FEET`<-as.numeric(gsub(",","", `LAND SQUARE FEET`))
 bronx1<-bronx1[which(bronx1$`GROSS SQUARE FEET`!=0 & bronx1$`LAND SQUARE FEET`!=0 & bronx1$`SALE\nPRICE`!=0),]
+attach(bronx1)
 plot(log(`GROSS SQUARE FEET`), log(`SALE\nPRICE`)) 
 m1<-lm(log(`SALE\nPRICE`)~log(`GROSS SQUARE FEET`))
 summary(m1)
 abline(m1,col="red",lwd=2)
 plot(resid(m1))
 
-m2<-lm(log(bronx1$SALE.PRICE)~log(bronx1$GROSS.SQUARE.FEET)+log(bronx1$LAND.SQUARE.FEET)+factor(bronx1$NEIGHBORHOOD))
+m2<-lm(log(bronx1$`SALE\nPRICE`)~log(bronx1$`GROSS SQUARE FEET`)+log(bronx1$`LAND SQUARE FEET`)+factor(bronx1$NEIGHBORHOOD))
 summary(m2)
 plot(resid(m2))
 # Suppress intercept - using "0+ ..."
-m2a<-lm(log(bronx1$SALE.PRICE)~0+log(bronx1$GROSS.SQUARE.FEET)+log(bronx1$LAND.SQUARE.FEET)+factor(bronx1$NEIGHBORHOOD))
-summary(m2a)
+m2a<-lm(log(bronx1$`SALE\nPRICE`)~0+log(bronx1$`GROSS SQUARE FEET`)+log(bronx1$`LAND SQUARE FEET`)+factor(bronx1$NEIGHBORHOOD))
 plot(resid(m2a))
 
-m3<-lm(log(bronx1$SALE.PRICE)~0+log(bronx1$GROSS.SQUARE.FEET)+log(bronx1$LAND.SQUARE.FEET)+factor(bronx1$NEIGHBORHOOD)+factor(bronx1$BUILDING.CLASS.CATEGORY))
+m3<-lm(log(bronx1$`SALE\nPRICE`)~0+log(bronx1$`GROSS SQUARE FEET`)+log(bronx1$`LAND SQUARE FEET`)+factor(bronx1$NEIGHBORHOOD)+factor(bronx1$`BUILDING CLASS CATEGORY`))
 summary(m3)
 plot(resid(m3))
 
-m4<-lm(log(bronx1$SALE.PRICE)~0+log(bronx1$GROSS.SQUARE.FEET)+log(bronx1$LAND.SQUARE.FEET)+factor(bronx1$NEIGHBORHOOD)*factor(bronx1$BUILDING.CLASS.CATEGORY))
+m4<-lm(log(bronx1$`SALE\nPRICE`)~log(bronx1$`GROSS SQUARE FEET`)+log(bronx1$`LAND SQUARE FEET`)+factor(bronx1$NEIGHBORHOOD)*factor(bronx1$`BUILDING CLASS CATEGORY`))
 summary(m4)
 plot(resid(m4))
+
+#bronx 2
+bronx1$SALE.PRICE<-sub("\\$","",bronx1$`SALE\nPRICE`) 
+bronx1$SALE.PRICE<-as.numeric(gsub(",","", bronx1$SALE.PRICE)) 
+bronx1$GROSS.SQUARE.FEET<-as.numeric(gsub(",","", bronx1$`GROSS SQUARE FEET`)) 
+bronx1$LAND.SQUARE.FEET<-as.numeric(gsub(",","", bronx1$`LAND SQUARE FEET`)) 
+bronx1$SALE.DATE<- as.Date(gsub("[^]:digit:]]","",bronx1$`SALE DATE`)) 
+bronx1$YEAR.BUILT<- as.numeric(gsub("[^]:digit:]]","",bronx1$`YEAR BUILT`)) 
+bronx1$ZIP.CODE<- as.character(gsub("[^]:digit:]]","",bronx1$`ZIP CODE`)) 
+
 
 ###Lab2
 
